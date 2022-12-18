975de01623d52e36de040fbf15ef4907a22a12a7
Andrey <andrey2004182@gmail.com>
2022-12-18 21:53:41 +0300

    Добавьте файлы проекта.

---
+++ TaskManager/Program.cs
@@ -0,0 +1,43 @@
+﻿using TaskManagerCommandsLib;
+namespace Program;
+class Program
+{
+    public static bool running = true;
+    static void Main()
+    {
+        Manager manager = new Manager();
+
+
+        while (running)
+        {
+
+
+
+            string command = Console.ReadLine();
+            ConsoleCommandExecute(command);
+            Console.WriteLine(manager.ExecuteCommand(command));
+
+
+
+        }
+    }
+    static void ConsoleCommandExecute(string comandName)
+    {
+
+        switch (comandName)
+        {
+            case "!exit":
+                running = false;
+                break;
+            case "!clear":
+                Console.Clear();
+                break;
+
+
+        }
+        //какие-то вспомогательные команды:
+        //сlear()
+        //exit()
+        //
+    }
+}
